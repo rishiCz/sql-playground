a1f26b7125465672c11415890fd16a117445ba04
Rishi Malik <rishimalik97@gmail.com>
2023-08-23 14:03:53 +0530

    Added CSV files and papaparse

--- src/components/tableHolder/index.js
+++ src/components/tableHolder/index.js
@@ -4,6 +4,7 @@ import Table from './table'
 const TableHolder = ()=>{
     return(
         <div>
+            <label className={styles.tableName}>Table 1</label>
             <Table/>
         </div>
     )
